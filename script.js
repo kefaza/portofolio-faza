950074bb59e480190325b353ca8ac96b82ff9313
kefaza <kefaza939@gmail.com>
2026-05-25 07:54:15 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const scrollup = document.querySelector(".scroll-up");
-
-window.onscroll = () => {
-    if (window.scrollY > 500) {
-        scrollup.classList.add("scroll-active");
-    } else {
-        scrollup.classList.remove("scroll-active");
-    }
-};
